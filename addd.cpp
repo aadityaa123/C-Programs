8ccc1f20e5073a32bff6d3c1d740e968fbdf8915
Aditya Agrawal <87859216+aadityaa123@users.noreply.github.com>
2022-11-21 12:31:09 +0530

    Create addd.cpp

---
+++ addd.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+main()
+{
+  int a,b,c;
+  cout<<"Enter the Value of a:- ";
+  cin>>a;
+  
+  cout<<"Enter the Value of b:- ";
+  cin>>b;
+  
+  c=a+b;
+  cout<<"Addition of a and b is:-"<<c;
+}
